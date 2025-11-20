8f8437bb0a33643ead6e6684acdc838b49519742
Jo-Sin <abrahamjosin21@gmail.com>
2025-11-21 01:28:14 +1100

    set up ninja api and postgres

---
+++ api/schemas.py
@@ -0,0 +1,7 @@
+from ninja import ModelSchema
+from .models import Trial
+
+class TrialBrief(ModelSchema):
+    class Meta:
+        model = Trial
+        fields = ['registration_number', 'public_title', 'recruitment_status', 'description', 'date_last_updated']
---
+++ api/views.py
@@ -0,0 +1,28 @@
+from django.db.models import QuerySet
+from django.http import HttpRequest
+from django.shortcuts import get_object_or_404
+from ninja import NinjaAPI
+from ninja.pagination import paginate
+ 
+from .models import Trial
+from .schemas import TrialBrief
+ 
+api = NinjaAPI(title="Dummy API for ANZCTR trial data")
+
+@api.get("/hello")
+def hello(request, name="world"):
+    print(name)
+    return "Hello {}!".format(name)
+ 
+@api.get("/trials", response=list[TrialBrief])
+@paginate
+def list_trials(request: HttpRequest) -> QuerySet[Trial]:
+    return Trial.objects.all()
+ 
+# @api.post("/books", response=BookOut)
+# def create_book(request: HttpRequest, payload: BookIn) -> Book:
+#     return Book.objects.create(**payload.model_dump())
+ 
+# @api.get("/books/{book_id}", response=BookOut)
+# def get_book(request: HttpRequest, book_id: int) -> Book:
+#     return get_object_or_404(Book, pk=book_id)
